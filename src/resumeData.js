c444434a45be29a810c0656208d66ee6af9f5224
Lakshay Verma <verma_lakshay@live.in>
2019-03-23 21:00:37 +0530

    Updated basic details

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,104 +1,126 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
+    "imagebaseurl":"https://lakshay.dev/",
+    "name": "Lakshay Verma",
+    "role": "Software Developer",
+    "linkedinId":"https://www.linkedin.com/in/lakshayverma/",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/lakshayverma/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"http://github.com/lakshayverma",
           "className":"fa fa-github"
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "name":"twitter",
+          "url":"http://twitter.com/lakshay__verma",
           "className":"fa fa-twitter"
         }
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
     "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "website":"https://lakshay.dev",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"I.G.N.O.U.",
+        "specialization":"Masters in Computer Application",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2017",
+        "Achievements":"Unity Game Development"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"G.N.D.U.",
+        "specialization":"Bachelors in Computer Application",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2014",
+        "Achievements":"Mobile Development"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Walkwel Technology",
+        "specialization":"Web Development and Datbases",
+        "MonthOfLeaving":"-",
+        "YearOfLeaving":"Present",
+        "Achievements":"PHP, Laravel, AWS, Click Funnels, Infusionsoft"
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
+        "CompanyName":"Club JB",
+        "specialization":"Web Development",
+        "MonthOfLeaving":"Sept",
         "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "Achievements":"PHP, Laravel, Android"
+      },
+      {
+        "CompanyName":"Walnut Studios",
+        "specialization":"Application and Game Development",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2018",
+        "Achievements":"PHP, Android, Unity, Adobe Photoshop"
+      },
+      {
+        "CompanyName":"Advait SparX",
+        "specialization":"Application and Game Development",
+        "MonthOfLeaving":"April",
+        "YearOfLeaving":"2017",
+        "Achievements":"Windows Phone, Unity, Adobe Photoshop"
       }
     ],
-    "skillsDescription":"Your skills here",
+    "skillsDescription":"Major Skills",
     "skills":[
       {
-        "skillname":"HTML5"
+        "skillname":"Laravel"
       },
       {
-        "skillname":"CSS"
+        "skillname":"Android"
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Adobe Photoshop"
       }
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Learning Management System",
+        "description":"Laravel Application that offers complete suite of Student to Trainer interaction towards course completion.",
+        "imgurl":"../images/lms.png"
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "name":"Amaze Yourself",
+        "description":"Mobile application based on a concept that amazes.",
+        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/amaze-your-self-logo.png"
       },
       {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
+        "name":"MarG",
+        "description":"Unity Mobile Game where users need to navigate through a series of mazes to reach goal.",
+        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/marg-logo.png"
       },
       {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Baani Nitnem",
+        "description":"Mobile application that helps in saying daily prayers.",
+        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/baani-nitnem-wide.png"
+      },
+      {
+        "name":"Birdy Blue",
+        "description":"Unity Mobile Game.",  
+        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/birdy-blue-wide.png"
       }
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Loved the application, Baani Nitnem. Helps me keep track of my daily prayers routine",
+        "name":"Pawanjot Singh"
       },
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"Awesome application, you nailed the graphics for the birdy.",
+        "name":"Simran Singh Sandhu"
       }
     ]
   }
