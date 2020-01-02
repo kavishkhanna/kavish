7b90bbe5077278d14bc25b94716c840af12f06e5
Lakshay Verma <verma_lakshay@live.in>
2020-01-02 21:05:57 +0530

    Update resume data

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,168 +1,180 @@
 let resumeData = {
-    "imagebaseurl":"https://lakshay.dev/",
-    "name": "Lakshay Verma",
-    "role": "Software Engineer",
-    "linkedinId":"https://www.linkedin.com/in/lakshayverma/",
-    "skypeid": "lk7253",
-    "roleDescription": "I like to work with database designs and backend. Also tweaking designs of images, websites is a hobby when not watching movies or playing games.",
-    "socialLinks":[
-        {
-          "id" : 1,
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/lakshayverma/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "id" : 2,
-          "name":"github",
-          "url":"http://github.com/lakshayverma",
-          "className":"fa fa-github"
-        },
-        {
-          "id" : 3,
-          "name":"twitter",
-          "url":"http://twitter.com/lakshay__verma",
-          "className":"fa fa-twitter"
-        },
-        {
-          "id" : 4,
-          "name":"instagram",
-          "url":"http://instagram.com/lakshay__verma",
-          "className":"fa fa-instagram"
-        },
-        {
-          "id" : 5,
-          "name":"blog",
-          "url":"http://blog.lakshay.dev",
-          "className":"fa fa-edit"
-        }
-      ],
-    "aboutme":"I am a developer by profession. Who like to see things through and loves to code. Watching anime, netflix and movies is a part time hobby apart from going out in the wild.",
-    "address":"India",
-    "website":"https://lakshay.dev",
-    "education":[
-      {
-        "id": 4,
-        "UniversityName":"I.G.N.O.U.",
-        "specialization":"Masters in Computer Application",
-        "MonthOfPassing":"Dec",
-        "YearOfPassing":"2017",
-        "Achievements":"Unity Game Development"
-      },
-      {
-        "id": 3,
-        "UniversityName":"G.N.D.U.",
-        "specialization":"Bachelors in Computer Application",
-        "MonthOfPassing":"Jul",
-        "YearOfPassing":"2014",
-        "Achievements":"Mobile Development"
-      }
-    ],
-    "work":[
-      {
-        "id": 4,
-        "CompanyName":"Walkwel Technology",
-        "specialization":"Web Development and Databases",
-        "MonthOfLeaving":"-",
-        "YearOfLeaving":"Present",
-        "Achievements":"PHP, Laravel, AWS, Click Funnels, Infusionsoft"
-      },
-      {
-        "id": 3,
-        "CompanyName":"Club JB",
-        "specialization":"Web Development",
-        "MonthOfLeaving":"Sept",
-        "YearOfLeaving":"2017",
-        "Achievements":"PHP, Laravel, Android"
-      },
-      {
-        "id": 2,
-        "CompanyName":"Walnut Studios",
-        "specialization":"Application and Game Development",
-        "MonthOfLeaving":"May",
-        "YearOfLeaving":"2017",
-        "Achievements":"PHP, Android, Unity, Adobe Photoshop"
-      },
-      {
-        "id" : 1,
-        "CompanyName":"Advait SparX",
-        "specialization":"Application and Game Development",
-        "MonthOfLeaving":"April",
-        "YearOfLeaving":"2016",
-        "Achievements":"Windows Phone, Unity, Adobe Photoshop"
-      }
-    ],
-    "skillsDescription":"Major Skills",
-    "skills":[
-      {
-        "id": 1,
-        "skillname":"Laravel",
-        "percent" : "80"
-      },
-      {
-        "id": 2,
-        "skillname":"Wordpress",
-        "percent" : "90"
-      },
-      {
-        "id": 3,
-        "skillname":"Android",
-        "percent" : "55"
-      },
-      {
-        "id": 4,
-        "skillname":"Java",
-        "percent" : "70"
-      },
-      {
-        "skillname":"Adobe Photoshop",
-        "percent" : "60"
-      }
-    ],
-    "portfolio":[
-      {
-        "id" : 1,
-        "name":"Learning Management System",
-        "description":"Laravel Application that offers complete suite of Student to Trainer interaction towards course completion.",
-        "imgurl":"../images/lms.png"
-      },
-      {
-        "id" : 2,
-        "name":"Amaze Yourself",
-        "description":"Mobile application based on a concept that amazes.",
-        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/amaze-your-self-logo.png"
-      },
-      {
-        "id": 3,
-        "name":"MarG",
-        "description":"Unity Mobile Game where users need to navigate through a series of mazes to reach goal.",
-        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/marg-logo.png"
-      },
-      {
-        "id" : 4,
-        "name":"Baani Nitnem",
-        "description":"Mobile application that helps in saying daily prayers.",
-        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/baani-nitnem-wide.png"
-      },
-      {
-        "id": 5,
-        "name":"Birdy Blue",
-        "description":"Unity Mobile Game.",
-        "imgurl":"https://blog.lakshay.dev/wp-content/uploads/2015/11/birdy-blue-wide.png"
-      }
-    ],
-    "testimonials":[
-      {
-        "id" : 1,
-        "name":"Pawanjot Singh",
-        "description":"Loved the application, Baani Nitnem. Helps me keep track of my daily prayers routine"
-      },
-      {
-        "id": 2,
-        "name":"Simran Singh Sandhu",
-        "description":"Awesome application, you nailed the graphics for the birdy. Keep going forward."
-      }
-    ]
-  }
+  "imagebaseurl": "https://lakshay.dev/",
+  "name": "Lakshay Verma",
+  "role": "Software Engineer",
+  "linkedinId": "https://www.linkedin.com/in/lakshayverma/",
+  "skypeid": "lk7253",
+  "roleDescription": "I like to work with database designs and backend. Also tweaking designs of images, websites is a hobby when not watching movies or playing games.",
+  "socialLinks": [
+    {
+      "id": 1,
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/lakshayverma/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "id": 2,
+      "name": "github",
+      "url": "http://github.com/lakshayverma",
+      "className": "fa fa-github"
+    },
+    {
+      "id": 3,
+      "name": "twitter",
+      "url": "http://twitter.com/lakshay__verma",
+      "className": "fa fa-twitter"
+    },
+    {
+      "id": 4,
+      "name": "instagram",
+      "url": "http://instagram.com/lakshay__verma",
+      "className": "fa fa-instagram"
+    },
+    {
+      "id": 5,
+      "name": "blog",
+      "url": "http://blog.lakshay.dev",
+      "className": "fa fa-edit"
+    }
+  ],
+  "aboutme": "I am a developer by profession. Who like to see things through and loves to code. Watching anime, netflix and movies is a part time hobby apart from going out in the wild.",
+  "address": "India",
+  "website": "https://lakshay.dev",
+  "education": [
+    {
+      "id": 4,
+      "UniversityName": "I.G.N.O.U.",
+      "specialization": "Masters in Computer Application",
+      "MonthOfPassing": "Dec",
+      "YearOfPassing": "2017",
+      "Achievements": "Unity Game Development"
+    },
+    {
+      "id": 3,
+      "UniversityName": "G.N.D.U.",
+      "specialization": "Bachelors in Computer Application",
+      "MonthOfPassing": "Jul",
+      "YearOfPassing": "2014",
+      "Achievements": "Mobile Development"
+    }
+  ],
+  "work": [
+    {
+      "id": 4,
+      "CompanyName": "Walkwel Technology",
+      "specialization": "Web Development and Databases",
+      "MonthOfLeaving": "-",
+      "YearOfLeaving": "Present",
+      "Achievements": "PHP, Laravel, AWS, Click Funnels, Infusionsoft"
+    },
+    {
+      "id": 3,
+      "CompanyName": "Club JB",
+      "specialization": "Web Development",
+      "MonthOfLeaving": "Sept",
+      "YearOfLeaving": "2017",
+      "Achievements": "PHP, Laravel, Android"
+    },
+    {
+      "id": 2,
+      "CompanyName": "Walnut Studios",
+      "specialization": "Application and Game Development",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2017",
+      "Achievements": "PHP, Android, Unity, Adobe Photoshop"
+    },
+    {
+      "id": 1,
+      "CompanyName": "Advait SparX",
+      "specialization": "Application and Game Development",
+      "MonthOfLeaving": "April",
+      "YearOfLeaving": "2016",
+      "Achievements": "Windows Phone, Unity, Adobe Photoshop"
+    }
+  ],
+  "skillsDescription": "Major Skills",
+  "skills": [
+    {
+      "id": 1,
+      "skillname": "Laravel",
+      "percent": "80",
+      "level": "intermediate"
+    },
+    {
+      "id": 2,
+      "skillname": "Wordpress",
+      "percent": "70",
+      "level": "intermediate"
+    },
+    {
+      "id": 3,
+      "skillname": "Android",
+      "percent": "55",
+      "level": "moderate"
+    },
+    {
+      "id": 4,
+      "skillname": "Java",
+      "percent": "60",
+      "level": "moderate"
+    },
+    {
+      "id": 5,
+      "skillname": "JavaScript",
+      "percent": "60",
+      "level": "moderate"
+    },
+    {
+      "id": 6,
+      "skillname": "Adobe Photoshop",
+      "percent": "60",
+      "level": "moderate"
+    }
+  ],
+  "portfolio": [
+    {
+      "id": 1,
+      "name": "Learning Management System",
+      "description": "Laravel Application that offers complete suite of Student to Trainer interaction towards course completion.",
+      "imgurl": "../images/lms.png"
+    },
+    {
+      "id": 2,
+      "name": "Amaze Yourself",
+      "description": "Mobile application based on a concept that amazes.",
+      "imgurl": "https://blog.lakshay.dev/wp-content/uploads/2015/11/amaze-your-self-logo.png"
+    },
+    {
+      "id": 3,
+      "name": "MarG",
+      "description": "Unity Mobile Game where users need to navigate through a series of mazes to reach goal.",
+      "imgurl": "https://blog.lakshay.dev/wp-content/uploads/2015/11/marg-logo.png"
+    },
+    {
+      "id": 4,
+      "name": "Baani Nitnem",
+      "description": "Mobile application that helps in saying daily prayers.",
+      "imgurl": "https://blog.lakshay.dev/wp-content/uploads/2015/11/baani-nitnem-wide.png"
+    },
+    {
+      "id": 5,
+      "name": "Birdy Blue",
+      "description": "Unity Mobile Game.",
+      "imgurl": "https://blog.lakshay.dev/wp-content/uploads/2015/11/birdy-blue-wide.png"
+    }
+  ],
+  "testimonials": [
+    {
+      "id": 1,
+      "name": "Pawanjot Singh",
+      "description": "Loved the application, Baani Nitnem. Helps me keep track of my daily prayers routine"
+    },
+    {
+      "id": 2,
+      "name": "Simran Singh Sandhu",
+      "description": "Awesome application, you nailed the graphics for the birdy. Keep going forward."
+    }
+  ]
+}
 
-  export default resumeData
+export default resumeData
