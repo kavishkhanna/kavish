24bf840c85700363735a0bea67ee4d7edaa0af7b
Lakshay Verma <verma_lakshay@live.in>
2020-01-02 21:24:35 +0530

    FIX key error in resume data

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -43,7 +43,7 @@ export default class Resume extends Component {
             {resumeData.work &&
               resumeData.work.map(item => {
                 return (
-                  <div className="row item" key={item.key}>
+                  <div className="row item" key={item.id}>
                     <div className="twelve columns">
                       <h3>{item.CompanyName}</h3>
                       <p className="info">
